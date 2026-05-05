2c98478c291ccaad10d91f9eafe3d963a9947e50
Ronnie Weinbach <ronniewe@modus.direct>
2026-05-05 11:03:19 +0300

    Create time_spine_daily.sql

---
+++ models/time_spine_daily.sql
@@ -0,0 +1,11 @@
+with date_spine as (
+  {{
+    dbt.date_spine(
+      datepart="day",
+      start_date="cast('2020-01-01' as date)",
+      end_date="cast('2035-01-01' as date)"
+    )
+  }}
+)
+select cast(date_day as date) as date_day
+from date_spine
